f85f0b20a8cd7963e4e746aff1902d4690865076
Nokarin (ノカリン ) <github@nokarin.my.id>
2026-04-26 18:52:21 +0700

    fix root wrapper scripts

--- linux/wrapper/root-wrapper.sh
+++ linux/wrapper/root-wrapper.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-exec "$scriptDir/share/dev.j7126.combat_tracker/combat_tracker" "$@"
+exec "$scriptDir/share/xyz.nokarin.frameextractor/FrameExtractor" "$@"
